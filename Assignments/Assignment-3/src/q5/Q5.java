2cab9cbf428d8050609799f88e1a29cd6f04cc6b
PrakharVishwakarma <prakharvishwapkv180@gmail.com>
2026-05-03 16:37:21 +0530

    Assignment 3:Q5

---
+++ Assignments/Assignment-3/src/q5/Q5.java
@@ -0,0 +1,39 @@
+package q5;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
+
+class Solution {
+	public ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		return slow;
+	}
+}
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
